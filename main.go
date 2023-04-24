7040cf4a1b228435f6ff8cc481c7908952176fc6
Benjamin Eskola <ben@eskola.uk>
2023-04-24 08:24:29 +0100

    Implement tracking of seen IDs

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"os"
+	"path/filepath"
 
 	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/adrg/xdg"
 	"github.com/mmcdole/gofeed"
 )
+
+var CACHEFILE string = filepath.Join(xdg.CacheHome, "rss2dayone.json")
 
 func main() {
 	fp := gofeed.NewParser()
@@ -18,12 +23,46 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Print(feed.Title)
+	processedAny := false
 
+	processed := loadProcessedItemsList()
 	for _, item := range feed.Items {
-		log.Print(item.GUID)
+		_, isPresent := processed[item.GUID]
+		if isPresent {
+			continue
+		}
+
 		processItem(item)
+
+		processed[item.GUID] = struct{}{}
+		processedAny = true
 	}
+
+	if processedAny {
+		saveProcessedItemsList(&processed)
+	}
+}
+
+func loadProcessedItemsList() map[string]struct{} {
+	// for compatibility with the python version this is saved as a list, but
+	// the go code wants it as a map for efficiency: O(1) instead of O(n)
+	processedList := make([]string, 0)
+
+	data, err := os.ReadFile(CACHEFILE)
+	if err == nil {
+		err = json.Unmarshal(data, &processedList)
+		if err != nil {
+			log.Fatal("Error decoding processed map:", err)
+		}
+	}
+
+	processed := make(map[string]struct{})
+	for _, i := range processedList {
+		// log.Print("seen: ", i)
+		processed[i] = struct{}{}
+	}
+
+	return processed
 }
 
 func processItem(item *gofeed.Item) {
@@ -42,5 +81,22 @@ func processItem(item *gofeed.Item) {
 
 	for n, ext := range item.Extensions["letterboxd"] {
 		log.Printf("%s\t%s\n", n, ext[0].Value)
+	}
+}
+
+func saveProcessedItemsList(processed *map[string]struct{}) {
+	processedList := make([]string, 0)
+	for i := range *processed {
+		processedList = append(processedList, i)
+	}
+
+	data, err := json.MarshalIndent(processedList, "", "  ")
+	if err != nil {
+		log.Fatal("Error serialising seen data:", err)
+	}
+
+	err = os.WriteFile(CACHEFILE, data, 0o644)
+	if err != nil {
+		log.Fatal("Error writing seen data to file:", err)
 	}
 }
