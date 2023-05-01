6a974086709a689ed045dad7d1b982f950589823
Benjamin Eskola <ben@eskola.uk>
2023-05-01 20:30:20 +0100

    Add tests for Post.FindAttachments

--- post_test.go
+++ post_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"testing"
 
+	"github.com/mmcdole/gofeed"
+	ext "github.com/mmcdole/gofeed/extensions"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -53,6 +55,82 @@ func TestRender(t *testing.T) {
 			}
 
 			assert.Equal(t, tc.expectedOutput, post.Render())
+		})
+	}
+}
+
+func TestFindAttachments(t *testing.T) { //nolint:funlen
+	t.Parallel()
+
+	testCases := []struct {
+		name           string
+		post           *Post
+		expectedOutput *map[string]struct{}
+	}{
+		{
+			name:           "no attachments",
+			post:           &Post{feedItem: &gofeed.Item{}}, //nolint:exhaustruct
+			expectedOutput: &map[string]struct{}{},
+		},
+		{
+			name: "with enclosure",
+			post: &Post{feedItem: &gofeed.Item{ //nolint:exhaustruct
+				Enclosures: []*gofeed.Enclosure{{URL: "http://test.invalid/hello.jpg"}},
+			}},
+			expectedOutput: &map[string]struct{}{"http://test.invalid/hello.jpg": {}},
+		},
+		{
+			name: "with media extension",
+			post: &Post{feedItem: &gofeed.Item{ //nolint:exhaustruct
+				Extensions: ext.Extensions{"media": {
+					"content": {
+						ext.Extension{Attrs: map[string]string{"url": "http://test.invalid/hello.jpg"}}, //nolint:exhaustruct
+					},
+				}},
+			}},
+			expectedOutput: &map[string]struct{}{"http://test.invalid/hello.jpg": {}},
+		},
+		{
+			name:           "with body link",
+			post:           &Post{feedItem: &gofeed.Item{}, body: "![](http://test.invalid/hello.jpg)"}, //nolint:exhaustruct
+			expectedOutput: &map[string]struct{}{"http://test.invalid/hello.jpg": {}},
+		},
+		{
+			name: "with body link and enclosure",
+			post: &Post{ //nolint:exhaustruct
+				feedItem: &gofeed.Item{ //nolint:exhaustruct
+					Enclosures: []*gofeed.Enclosure{{URL: "http://test.invalid/hello.jpg"}},
+				},
+				body: "![](http://test.invalid/anotherpicture.jpg)",
+			},
+			expectedOutput: &map[string]struct{}{
+				"http://test.invalid/hello.jpg":          {},
+				"http://test.invalid/anotherpicture.jpg": {},
+			},
+		},
+		{
+			name: "with body link duplicating an enclosure",
+			post: &Post{ //nolint:exhaustruct
+				feedItem: &gofeed.Item{ //nolint:exhaustruct
+					Enclosures: []*gofeed.Enclosure{{URL: "http://test.invalid/hello.jpg"}},
+				},
+				body: "![](http://test.invalid/hello.jpg)",
+			},
+			expectedOutput: &map[string]struct{}{
+				"http://test.invalid/hello.jpg": {},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc //nolint:varnamelen
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			tc.post.FindAttachments()
+
+			assert.Equal(t, tc.expectedOutput, tc.post.attachmentUrls)
 		})
 	}
 }
